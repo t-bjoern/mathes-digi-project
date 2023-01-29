4ccbc71d9314e0ae60b2441d55225e64ae7fa624
t-bjoern <thorbjoern.putzar@web.de>
2023-01-29 17:21:35 +0100

    Angefangen zu testen (SHK-7)

--- mathesdigi_app/tests.py
+++ mathesdigi_app/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from urllib.parse import urlencode
+import pytest
+from django.urls import reverse
+from mathesdigi_app.models import User
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_user_create():
+    User.objects.create(user_name="Test", mail="test@mail.de")
+    assert User.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_registration_view(client):
+    url = reverse("registration")
+    data = urlencode({'csrfmiddlewaretoken': 'something',
+                      'user_name': 'something',
+                      'mail': 'something_2'})
+    response = client.post(url, data, content_type="application/x-www-form-urlencoded")
+    print(response)
+    print(User.objects.get(user_name="something"))
+    assert False
