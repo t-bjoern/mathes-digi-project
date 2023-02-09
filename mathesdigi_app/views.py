a47ddcca83525679ff2bfd94799bb15befe53c64
t-bjoern <thorbjoern.putzar@web.de>
2023-02-09 22:28:07 +0100

    Change main_view

--- mathesdigi_app/views.py
+++ mathesdigi_app/views.py
@@ -35,7 +35,6 @@ def registration(request):
                 request.session["user"] = user.id
                 user.heft = request.session["heft"]
                 user.save()
-
             return redirect(reverse('main_view', args=(request.session["heft"], "1_example")))
         except Exception as e:
             # Context füllen, um Daten und Fehlermeldungen im Fomrular anzzuzeigen.
@@ -54,26 +53,23 @@ def registration(request):
 
 
 def main_view(request, heft, next_task_name):
-    context = {}
     if request.method == 'POST':
         post_data = dict(request.POST).copy()
+        # Convert lists in dict to single values
         post_data = {key: value[0] for key, value in post_data.items()}
         # Delete unnecessary information in post_data
         del post_data["csrfmiddlewaretoken"]
         this_task_process = post_data["this_task_process"]
-        is_next_example = helpers.str2bool(post_data["is_next_example"])
         user_id = request.session["user"]
 
-        if is_next_example:
-            example_id = post_data["example_id"]
-            context = helpers.get_example_solution(example_id)
-        if this_task_process == "task" and isinstance(post_data["input"], int):
+        if this_task_process == "example":
+            pass
+        elif this_task_process == "task_normal":
             teilaufgaben_id = post_data["task_id"]
             ergebnis = int(post_data["input"])
             helpers.save_answer(teilaufgaben_id, ergebnis, user_id)
-        if this_task_process == "drag_and_drop":
-            # preprocess for save drag and drop answer
-            # ...
-            # helpers.save_answer(post_data, user_id)
+        elif this_task_process == "drag_and_drop":
+            # preprocess ...
+            # helpers.save_answer(teilaufgaben_id, ergebnis, user_id)
             pass
-    return render(request, f'mathesdigi_app/{heft}/{next_task_name}.html', context)
+    return render(request, f'mathesdigi_app/{heft}/{next_task_name}.html')
