9da902386d783c352a6cfd6f14203fdb22ac1885
Emely-K <emelykuschel@gmail.com>
2023-02-21 14:34:40 +0100

    remove old pics and old 1_example.html and old 1_task_1.html

--- mathesdigi_app/static/mathesdigi_app/JS/validation.js
+++ mathesdigi_app/static/mathesdigi_app/JS/validation.js
@@ -1,6 +1,10 @@
+/** This function is designed to validate a single input field with the ID kids_answer_1.
+ *  The function checks if the input field is empty and if so, adds a CSS class input_field_shake
+ *  to create a shaking effect to draw the user's attention.
+ *  It then removes this class after a delay of 1 second using setTimeout().*/
 function validateForm_singleAnswer() {
-    var x = document.getElementById("kids_answer_1").value;
-    if (x.trim() === "") {
+    const kids_answer = document.getElementById("kids_answer_1").value;
+    if (kids_answer.trim() === "") {
         document.getElementById("kids_answer_1").classList.add("input_field_shake");
         setTimeout(function () {
             document.getElementById("kids_answer_1").classList.remove("input_field_shake");
