5a79ca11582c028dc77ff7379ee20135e5084893
Emely-K <emelykuschel@gmail.com>
2023-02-21 16:32:45 +0100

    Check solution 1_example.html

--- mathesdigi_app/static/mathesdigi_app/JS/validation.js
+++ mathesdigi_app/static/mathesdigi_app/JS/validation.js
@@ -1,17 +1,40 @@
-/** This function is designed to validate a single input field with the ID kids_answer_1.
+/** This function is designed to validate a single input field with the ID kids_answer_input_field_1.
  *  The function checks if the input field is empty and if so, adds a CSS class input_field_shake
  *  to create a shaking effect to draw the user's attention.
  *  It then removes this class after a delay of 1 second using setTimeout().*/
 function validateForm_singleAnswer() {
-    const kids_answer = document.getElementById("kids_answer_1").value;
-    if (kids_answer.trim() === "") {
-        document.getElementById("kids_answer_1").classList.add("input_field_shake");
+    const kids_answer_input_field = document.getElementById("kids_answer_1");
+    if (kids_answer_input_field.value.trim() === "") {
+        kids_answer_input_field.classList.add("input_field_shake");
+        kids_answer_input_field.classList.remove("input_field_shake");
         setTimeout(function () {
-            document.getElementById("kids_answer_1").classList.remove("input_field_shake");
-        }, 1000);
+            kids_answer_input_field.classList.remove("input_field_shake");
+        }, 800);
         return false;
     } else {
-        document.getElementById("kids_answer_1").classList.remove("input_field_shake");
+        kids_answer_input_field.classList.remove("input_field_shake");
         return true;
     }
+}
+
+function validateForm_example_singleAnswer(solution) {
+    validateForm_singleAnswer()
+    const kids_answer_input_field = document.getElementById("kids_answer_1");
+
+    if (kids_answer_input_field.value === solution) {
+        kids_answer_input_field.style.color = 'green';
+        setTimeout(function () {
+            document.getElementById("myform").submit();
+        }, 3000);
+    } else {
+        kids_answer_input_field.style.color = 'red';
+        setTimeout(function () {
+            kids_answer_input_field.value = solution;
+            kids_answer_input_field.style.color = 'green';
+        }, 1000);
+        setTimeout(function () {
+            document.getElementById("myform").submit();
+        }, 4000);
+    }
+    return false;
 }
