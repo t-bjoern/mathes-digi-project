ee34a6fad9525424c708562c28a8e1a77abc10f3
Emely-K <emelykuschel@gmail.com>
2023-02-22 16:27:46 +0100

    documentation single_answer.js

--- mathesdigi_app/static/mathesdigi_app/JS/single_answer.js
+++ mathesdigi_app/static/mathesdigi_app/JS/single_answer.js
@@ -1,7 +1,6 @@
 let last_clicked_input_field_id
 
-/**This code listens for the DOM to load and then sets a reference for each input-field.
- It then sets an event listener on that input field for the "keydown" and "click" event.*/
+/**This code listens for the DOM to load. After that it adds EventListener for clicking, keyboard-inputs.*/
 document.addEventListener("DOMContentLoaded", function () {
     const inputFields = document.querySelectorAll("input[type='text']");
     inputFields.forEach(function (inputField) {
@@ -13,10 +12,8 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 /** Action for keyboard-input
- The function attached to the event listener checks the pressed key, if it is a number between 0 and 9 and
- the length of the input value is less than 3, it allows the key press to go through. If the pressed key is
- the backspace key, it also allows the key press to go through. In all other cases, it prevents the default
- behavior of the key press.*/
+ *  Only keys 0-9 and Backspace are allowed. Only three digits can be written.
+ */
 function keyboard_add_number(event) {
     if (event.key >= "0" && event.key <= "9" && this.value.length < 3) {
     } else if (event.key === "Backspace") {
@@ -26,11 +23,10 @@ function keyboard_add_number(event) {
 }
 
 /** Action for number-buttons
- The information about the clicked buttons is stored in stat.
- If no input-field has been clicked, the default-input field "kids_answer_1" will be selected.
- Otherwise, if the user already clicked an input-field the id of that field will be stored in lastclickedinputfield_id.
- If the clicked button id is "p_delete" the input-field is going to be set to ''. If the value of the input-field has
- fewer than 3 digits, the function appends the value of the clicked button (stat) to the target (input-field).*/
+ * The value of the clicked button is appended to the text of the last clicked target. Only three digits are allowed.
+ * If no input-field had been clicked it will automatically start writing in the first field.
+ *
+ * The information about the clicked buttons is stored in stat.*/
 function buttons_add_number(stat) {
     if (typeof last_clicked_input_field_id === "undefined") {
         last_clicked_input_field_id = "kids_answer_1"
