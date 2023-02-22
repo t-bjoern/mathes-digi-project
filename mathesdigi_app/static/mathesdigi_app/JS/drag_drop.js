1af1357a6594a3b95c9e79256acb86071299faf1
Emely-K <emelykuschel@gmail.com>
2023-02-22 15:48:52 +0100

    rename zielzone -> dropzone drag and drop works for laptops (missing functionality for two already moved elements)

--- mathesdigi_app/static/mathesdigi_app/JS/drag_drop.js
+++ mathesdigi_app/static/mathesdigi_app/JS/drag_drop.js
@@ -10,6 +10,13 @@ document.addEventListener("DOMContentLoaded", function () {
         });
         answer.addEventListener("keydown", keyboard_add_number_div);
     });
+
+    for (const draggable of document.querySelectorAll("[draggable=true]")) {
+        draggable.addEventListener("dragstart", dragStart);
+    }
+    for (const dropzone of document.querySelectorAll(".dropzone")) {
+        dropzone.addEventListener("drop", drop);
+    }
 });
 
 /** Action for keyboard-input
@@ -59,43 +66,66 @@ function buttons_add_number(stat) {
     }
 }
 
-//
-// let lastclickedid;
-//
-// //add EventListeners for drop, dragover, dragstart and pressed keys
-// document.addEventListener("DOMContentLoaded", function () {
-//     for (const zielzone of document.querySelectorAll(".zielzone")) {
-//         zielzone.addEventListener("drop", drop);
-//         // zielzone.addEventListener("dragover", dragover);
-//
-//     }
-//     for (const draggable of document.querySelectorAll("[draggable=true]")) {
-//         draggable.addEventListener("dragstart", dragstart);
-//     }
-//
-// });
-//
-// function dragstart(event) {
-//     event.dataTransfer.setData('text', event.target.id);
-// }
-//
-// //function dragover(event) {event.preventDefault();}
-//
-// function drop(event) {
-//     // overwrite existing method
-//     event.preventDefault();
-//     const book_id = event.dataTransfer.getData('text');
-//     const target = event.currentTarget;
-//
-//     if (target.innerText !== '') {
-//         target.innerText = ''
-//     }
-//
-//     //remove already dropped book
-//     if (target.hasChildNodes()) {
-//         unsorted_books.appendChild(target.firstChild);
-//     }
-//
-//     // add book
-//     target.appendChild(document.getElementById(book_id));
-// }
+function dragStart(event) {
+    // Setzen der Daten, die beim Ablegen des Elements verwendet werden
+    const child_id = event.target.id;
+    const child_element = document.getElementById(child_id);
+    const parent_element = child_element.parentNode;
+
+    // set ids for child and parent
+    event.dataTransfer.setData("moved_element_id", child_id);
+    event.dataTransfer.setData('parent_id', parent_element.id)
+
+    // TODO entfernen
+    // Setzen des Effekts, der beim Ablegen des Elements angezeigt wird
+    // event.dataTransfer.dropEffect = "move";
+}
+
+function drop(event) {
+    event.preventDefault();
+
+    // get ids (child and parent), target and task_pics-element
+    const target = event.currentTarget;
+    const unsorted_task_pics = document.getElementById('task_pics');
+    const moved_element_id = event.dataTransfer.getData("moved_element_id");
+    const parent_id = event.dataTransfer.getData('parent_id');
+
+    // reset text-inputs
+    if (target.innerText !== '') {
+        target.innerText = ''
+    }
+
+    if (parent_id === 'task_pics') {
+        //remove already dropped element
+        if (target.hasChildNodes()) {
+            unsorted_task_pics.appendChild(target.firstChild);
+
+        } else {
+            // create placeholder Element
+            unsorted_task_pics.appendChild(createPlaceholderElement(moved_element_id));
+        }
+    }
+
+    // TODO Ablauf wenn zwei liegende Bücher aufeinander geschoben werden (Aktuell ist es so, dass dann beide Bücher in einem Feld landen.)
+    // add child to target
+    target.appendChild(document.getElementById(moved_element_id));
+
+    // TODO entfernen
+    // Entfernen Sie das verschobene Element
+    // unsorted_task_pics.removeChild(verschobenesElement);0
+    // let parentNode = document.getElementById(moved_element_id).parentNode
+    // alert(parentNode.id)
+    // element.addEventListener("dragstart", dragStart);
+}
+
+function createPlaceholderElement(moved_element_id) {
+    const verschobenesElement = document.getElementById(moved_element_id);
+    const neuesElement = document.createElement("div");
+    neuesElement.style.width = verschobenesElement.offsetWidth + "px";
+    neuesElement.style.height = verschobenesElement.offsetHeight + "px";
+    // TODO entfernen
+    // neuesElement.classList.add('empty');
+    // TODO color = transparent
+    neuesElement.style.backgroundColor = "red";
+    return neuesElement
+}
